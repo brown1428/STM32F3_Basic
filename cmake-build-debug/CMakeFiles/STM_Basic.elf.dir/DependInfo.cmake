339652c486057b31c61f7edc4dd336d6059bee8d
brown1428 <brown1428@gmail.com>
2022-01-02 22:14:26 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/STM_Basic.elf.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Startup/startup_stm32f303vctx.s" "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/cmake-build-debug/CMakeFiles/STM_Basic.elf.dir/Startup/startup_stm32f303vctx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F303xC"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_exti.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pcd_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_spi_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usb.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/main.c" "CMakeFiles/STM_Basic.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/main.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/stm32f3xx_hal_msp.c" "CMakeFiles/STM_Basic.elf.dir/Src/stm32f3xx_hal_msp.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/stm32f3xx_hal_msp.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/stm32f3xx_it.c" "CMakeFiles/STM_Basic.elf.dir/Src/stm32f3xx_it.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/stm32f3xx_it.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/syscalls.c" "CMakeFiles/STM_Basic.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/syscalls.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/sysmem.c" "CMakeFiles/STM_Basic.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/sysmem.c.obj.d"
+  "/Users/macbookpro14/Dropbox/Coding/C/STM32/STM_Basic/Src/system_stm32f3xx.c" "CMakeFiles/STM_Basic.elf.dir/Src/system_stm32f3xx.c.obj" "gcc" "CMakeFiles/STM_Basic.elf.dir/Src/system_stm32f3xx.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
